798a5992ac755d45349aa7f1224c7921561c281b
Sergey_Trotsenko <advocate.tsv@gmail.com>
2021-12-19 23:39:49 +0600

    Add description and new method

--- Web/Services/IPlayGroundService.cs
+++ Web/Services/IPlayGroundService.cs
@@ -7,14 +7,33 @@ namespace Web.Services
     /// </summary>
     public interface IPlayGroundService
     {
-        void SetNewPlayGround(int numberOfPsychics);
-
+        /// <summary>
+        /// Пытается получить PlayGround из Session.
+        /// </summary>
+        /// <param name="playGroundInterface">Интерфейс игровой площадки</param>
+        /// <returns>True если значение в Session имеется</returns>
         bool TryGetPlayGround(out IPlayGround playGround);
 
+        /// <summary>
+        /// Инициализирует новую игровую площадку.
+        /// </summary>
+        /// <param name="numberOfPsychics">Количество экстрасенсов</param>
+        IPlayGroundService CreateNewPlayGround(int numberOfPsychics);
+
+        /// <summary>
+        /// Устанавливает в имеющийся PlayGround значение загаданного числа.
+        /// </summary>
+        /// <param name="desiredValue">Загаданное число</param>
         IPlayGroundService SetNextDesiredValue(int desiredValue);
 
+        /// <summary>
+        /// Запускает в PlayGround ход участника или экстрасенсов (в порядке очередности).
+        /// </summary>
         IPlayGroundService Run();
 
+        /// <summary>
+        /// Обновляет PlayGround в Session.
+        /// </summary>
         void UpdateSession();
     }
 }
